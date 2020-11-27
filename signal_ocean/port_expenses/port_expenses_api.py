6798dfc45191a971e2613edfa87e90ac948aef27
Kimon Tsitsikas <k.tsitsikas@thesignalgroup.com>
2020-11-27 17:54:32 +0200

    POOL-1105 added model vessel methods

--- signal_ocean/port_expenses/port_expenses_api.py
+++ signal_ocean/port_expenses/port_expenses_api.py
@@ -101,3 +101,81 @@ class PortExpensesAPI:
             if response_json else None
 
         return return_object
+
+    def get_port_model_vessel_expenses(
+        self, port_id: int, vessel_type_id: int,
+            formula_calculation_date: datetime, vessel_class_id: int = 0,
+            operation_status: int = 0, historical_tce: bool = False,
+            estimation_status: int = 0) -> Optional[PortExpenses]:
+        """Retrieves model vessel port expenses.
+
+        Args:
+            port_id
+            vessel_type_id
+            formula_calculation_date
+            vessel_class_id
+            operation_status
+            historical_tce
+            estimation_status
+
+        Returns:
+            The port expenses for model vessel or None if a port with given ID
+            does not exist or a vessel type with the given ID number does not
+            exist.
+        """
+        query_string: QueryString = {
+            "portId": '{}'.format(port_id),
+            "vesselTypeId": '{}'.format(vessel_type_id),
+            "formulaCalculationDate": formula_calculation_date.isoformat(),
+            "vesselClassId": '{}'.format(vessel_class_id),
+            "operationStatus": '{}'.format(operation_status),
+            "historicalTce": '{}'.format(historical_tce),
+            "estimationStatus": '{}'.format(estimation_status)
+        }
+
+        response = self.__connection._make_post_request(
+            "port-expenses/api/v1/PortModelVessel", query_string
+        )
+        response.raise_for_status()
+        response_json = response.json()
+        return_object = parse_port_expenses(response_json) \
+            if response_json else None
+
+        return return_object
+
+    def get_canal_model_vessel_expenses(
+        self, canal: int, open_port_id: int, load_port_id: int,
+            discharge_port_id: int, operation_status: int,
+            formula_calculation_date: datetime) -> Optional[CanalExpenses]:
+        """Retrieves model vessel canal expenses.
+
+        Args:
+            canal
+            open_port_id
+            load_port_id
+            discharge_port_id
+            operation_status
+            formula_calculation_date
+
+        Returns:
+            The canal expenses for model vessel or None if expenses can be
+            provided for the given parameters.
+        """
+        query_string: QueryString = {
+            "canal": '{}'.format(canal),
+            "openPortId": '{}'.format(open_port_id),
+            "loadPortId": '{}'.format(load_port_id),
+            "dischargePortId": '{}'.format(discharge_port_id),
+            "operationStatus": '{}'.format(operation_status),
+            "formulaCalculationDate": formula_calculation_date.isoformat()
+        }
+
+        response = self.__connection._make_post_request(
+            "port-expenses/api/v1/CanalModelVessel", query_string
+        )
+        response.raise_for_status()
+        response_json = {"TotalCost": response.json()}
+        return_object = parse_canal_expenses(response_json) \
+            if response_json else None
+
+        return return_object
--- tests/port_expenses/test_port_expenses_api.py
+++ tests/port_expenses/test_port_expenses_api.py
@@ -71,3 +71,66 @@ def test_get_canal_expenses(canal, imo, open_port_id, load_port_id,
             "cargoType": cargo_type
         },
     )
+
+
+@pytest.mark.parametrize('port_id, vessel_type_id, '
+                         'formula_calculation_date', [
+    (3689, 3, datetime(2020, 6, 8, 0, 0, 0)),
+    (3153, 4, datetime(2020, 6, 12, 0, 0, 0)),
+    (3324, 5, datetime(2020, 3, 8, 0, 0, 0))
+])
+def test_get_port_model_vessel_expenses(port_id, vessel_type_id,
+                                        formula_calculation_date):
+    connection = MagicMock()
+    api = PortExpensesAPI(connection)
+
+    pe_object = api.get_port_model_vessel_expenses(port_id, vessel_type_id,
+                                                   formula_calculation_date)
+
+    assert type(pe_object) is PortExpenses
+
+    connection._make_post_request.assert_called_with(
+        "port-expenses/api/v1/PortModelVessel",
+        {
+            "portId": '{}'.format(port_id),
+            "vesselTypeId": '{}'.format(vessel_type_id),
+            "formulaCalculationDate": formula_calculation_date.isoformat(),
+            'vesselClassId': '0',
+            'operationStatus': '0',
+            'historicalTce': 'False',
+            'estimationStatus': '0'
+        },
+    )
+
+
+@pytest.mark.parametrize('canal, open_port_id, load_port_id, '
+                         'discharge_port_id, operation_status, '
+                         'formula_calculation_date', [
+    (2, 3758, 3758, 3617, 1, datetime(2018, 1, 8, 5, 26, 36)),
+    (1, 3773, 3360, 3794, 1, datetime(2020, 2, 27, 17, 48, 11))
+])
+def test_get_canal_model_vessel_expenses(canal, open_port_id, load_port_id,
+                                         discharge_port_id, operation_status,
+                                         formula_calculation_date):
+    connection = MagicMock()
+    api = PortExpensesAPI(connection)
+
+    canal_expenses = api.get_canal_model_vessel_expenses(canal, open_port_id,
+                                                         load_port_id,
+                                                         discharge_port_id,
+                                                         operation_status,
+                                                         formula_calculation_date)
+
+    assert type(canal_expenses) is CanalExpenses
+
+    connection._make_post_request.assert_called_with(
+        "port-expenses/api/v1/CanalModelVessel",
+        {
+            "canal": '{}'.format(canal),
+            "openPortId": '{}'.format(open_port_id),
+            "loadPortId": '{}'.format(load_port_id),
+            "dischargePortId": '{}'.format(discharge_port_id),
+            "operationStatus": '{}'.format(operation_status),
+            "formulaCalculationDate": formula_calculation_date.isoformat()
+        },
+    )
