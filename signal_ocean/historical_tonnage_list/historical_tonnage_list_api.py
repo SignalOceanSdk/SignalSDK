5ebdca958a2a1baa0135304465e4d7fdf4871b01
LOCAL\p.rompanos <p.rompanos@thesignalgroup.com>
2020-12-05 00:06:32 +0200

    Some more changes

--- signal_ocean/historical_tonnage_list/historical_tonnage_list_api.py
+++ signal_ocean/historical_tonnage_list/historical_tonnage_list_api.py
@@ -11,8 +11,7 @@ from . import _historical_tonnage_list_json
 
 
 class HistoricalTonnageListAPI:
-    """Handles communications with  Signal's Historical Tonnage List API.
-    """
+    """Handles communications with  Signal's Historical Tonnage List API."""
 
     def __init__(self, connection: Optional[Connection] = None):
         """Initializes HistoricalTonnageListAPI.
