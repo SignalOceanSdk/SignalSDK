604af9019abdb126700827dda49e335c9c220889
AzureAD\TomaszKlepacki <tomasz.klepacki@jit.team>
2021-03-10 09:54:12 +0100

    Fixing path to jupyter

--- nbrunner.sh
+++ nbrunner.sh
@@ -1,2 +1,2 @@
 ls -LR | grep .ipynb | while read filename; 
-do jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"; done
+do ~/.local/bin/jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"; done
