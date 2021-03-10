ac7e621b5e3573027732478cf9e403dea97c03d3
AzureAD\TomaszKlepacki <tomasz.klepacki@jit.team>
2021-03-10 22:47:54 +0100

    Fixes in shell script

--- nbrunner.sh
+++ nbrunner.sh
@@ -4,7 +4,6 @@ ls -LR | grep .ipynb | while read filename;
 do
 env_name=SIGNAL_OCEAN_API_KEY;
 apikey=$(printf '%s\n' "${!env_name}");
-echo $apikey
 
 if [ -z "$apikey" ]; 
     then echo 'Error: SIGNAL_OCEAN_API_KEY has not been defined';
@@ -17,6 +16,7 @@ sed -i 's/subscription_key = \\"\\"/subscription_key = \\"'$apikey'\\"/' docs/ex
 sed -i "s/subscription_key = ''/subscription_key = '$apikey'/" docs/examples/jupyter/*/"$filename";
 sed -i "s/signal_ocean_api_key = ''/signal_ocean_api_key = '$apikey'/" docs/examples/jupyter/*/"$filename";
 sed -i "s/signal_ocean_api_key = 'NotValid'/signal_ocean_api_key = '$apikey'/" docs/examples/jupyter/*/"$filename";
+sed -i "s/signal_ocean_api_key = 'Not Valid'/signal_ocean_api_key = '$apikey'/" docs/examples/jupyter/*/"$filename";
 
 ~/.local/bin/jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"; 
 done
