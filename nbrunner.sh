e94eb02600aa67550d40fd08d5998d13a30c4523
AzureAD\TomaszKlepacki <tomasz.klepacki@jit.team>
2021-03-15 14:47:20 +0100

    Cleanin up script

--- nbrunner.sh
+++ nbrunner.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 ls -LR | grep .ipynb | while read filename;
 do
-env_name=SIGNAL_OCEAN_API_KEY;
-apikey=$(printf '%s\n' "${!env_name}");
 
-if [ -z "$apikey" ]; 
+if [ -z "${SIGNAL_OCEAN_API_KEY}" ]; 
     then echo 'Error: SIGNAL_OCEAN_API_KEY has not been defined';
     exit 1;
 fi
 
 echo "$filename";
-~/.local/bin/jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"; 
+jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"; 
 done
