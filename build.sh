fe380281356b53e4df0ae4d5236f7629f2f97410
gmema <g.mema@thesignalgroup.com>
2022-03-08 12:04:40 +0200

    change build file

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ echo 'Activating virtual environment...'
 source ./env/bin/activate
 
 echo 'Installing dependencies...'
-pip install -r ./requirements.txt
+pip3 install -r ./requirements.txt
 
 echo 'Running type checks...'
 mypy --strict signal_ocean
