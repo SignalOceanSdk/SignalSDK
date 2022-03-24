3ed044eb58d14f3ea3b3619a67ed1324a0399dc8
k.ginis <k.ginis@thesignalgroup.com>
2022-03-24 20:12:00 +0200

    [DEVOPS-752] New .sh scripts to accommodate TC pipeline reconfiguration

---
+++ nbrunner-docker.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+pip install notebook matplotlib openpyxl seaborn folium sqlalchemy pandas --upgrade
+pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple signal-ocean
+
+ls -LR | grep .ipynb | while read filename
+do
+
+if [ -z "${SIGNAL_OCEAN_API_KEY}" ]
+    then echo 'Error: SIGNAL_OCEAN_API_KEY has not been defined'
+    exit 1
+fi
+
+echo "$filename"
+jupyter nbconvert --execute --clear-output docs/examples/jupyter/*/"$filename"
+done
