caf173467df9f192205cff4db774582d9e755c3a
Konstantinos Ginis <k.ginis@thesignalgroup.com>
2022-05-20 17:39:10 +0300

    Added plotly library in pip install command

--- nbrunner-docker.sh
+++ nbrunner-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -euxo pipefail
 
-pip install notebook jupyter matplotlib openpyxl seaborn folium sqlalchemy pandas --upgrade
+pip install notebook jupyter plotly matplotlib openpyxl seaborn folium sqlalchemy pandas --upgrade
 pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple signal-ocean
 
 ls -LR | grep .ipynb | while read filename
