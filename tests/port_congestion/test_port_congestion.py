52dbc73fbca6e965f3b628a0844a07d5dd870236
jgmoutafis <jg2moutafis@gmail.com>
2023-03-27 12:55:55 +0300

    Port Congestion API integration.

--- tests/port_congestion/test_port_congestion.py
+++ tests/port_congestion/test_port_congestion.py
@@ -1,30 +1,15 @@
-from datetime import date, datetime, timezone
-from typing import Tuple, List
-from unittest.mock import MagicMock
-from urllib.parse import urljoin
-from freezegun import freeze_time
-
-import pytest
-import pandas as pd
-from pandas.testing import assert_frame_equal
+import datetime
 import json
+from datetime import date
+from unittest.mock import MagicMock
 
-from signal_ocean import Connection
+import pandas as pd
+from freezegun import freeze_time
+from pandas.testing import assert_frame_equal
+
 from signal_ocean.port_congestion import PortCongestion
-from signal_ocean.voyages.models import (
-    Voyage,
-    VoyageEvent,
-    VoyageEventDetail,
-    VoyageGeo,
-    VoyagesFlat,
-)
-from signal_ocean.port_congestion.models import (
-    LiveCongestion,
-    NumberOfVesselsOverTime,
-    WaitingTimeOverTime,
-    VesselsCongestionData,
-)
-
+from signal_ocean.port_congestion.models import PortCongestionQueryResponse
+from signal_ocean.util.parsing_helpers import parse_model
 
 _mock_voyages = pd.read_csv("tests/port_congestion/Data/mock_voyages.csv")
 _mock_events = pd.read_csv(
@@ -72,6 +57,9 @@ _mock_live_port_congestion = pd.read_csv(
     ],
 )
 
+with open("tests/port_congestion/Data/mock_port_congestion_api_response.json") as fp:
+    _mock_port_congestion_api_response = json.load(fp)
+
 _port_congestion = PortCongestion()
 
 _port_congestion._get_voyages_data = MagicMock(
@@ -83,9 +71,14 @@ _port_congestion._get_voyages_data = MagicMock(
     )
 )
 
+_parsed_congestion_api_mock_response = parse_model(_mock_port_congestion_api_response, PortCongestionQueryResponse)
+_port_congestion._port_congestion_api.query_port_congestion = MagicMock(
+    return_value=_parsed_congestion_api_mock_response.time_series
+)
+
+
 @freeze_time("2022-03-03")
 def test_preprocess_voyages_data():
-
     (
         voyages_df,
         events_df,
@@ -101,6 +94,7 @@ def test_preprocess_voyages_data():
         _mock_vessels_congestion_data, vessels_congestion_data
     )
 
+
 @freeze_time("2022-03-03")
 def test_number_of_vessels_over_time():
     number_of_vessels_over_time = (
@@ -113,17 +107,27 @@ def test_number_of_vessels_over_time():
         _mock_number_of_vessels_over_time, number_of_vessels_over_time
     )
 
-@freeze_time("2022-03-03")
+
 def test_waiting_time_over_time():
+    vessel_class_id = 70
+    date_from = datetime.datetime(2022, 3, 3)
+    waiting_df = _mock_vessels_congestion_data.loc[_mock_vessels_congestion_data["mode"] == "Waiting"]
+    ports = waiting_df["port_name_geos"].unique().tolist()
+    level_0_areas = waiting_df["area_name_level0_geos"].unique().tolist()
+
     waiting_time_over_time = (
-        _port_congestion._calculate_waiting_time_over_time(
-            _mock_vessels_congestion_data
+        _port_congestion._get_waiting_time_over_time(
+            congestion_start_date=date_from,
+            vessel_class_id=vessel_class_id,
+            ports=ports,
+            areas=level_0_areas,
         )
     )
 
     assert not assert_frame_equal(
         _mock_waiting_time_over_time, waiting_time_over_time
     )
+
 
 @freeze_time("2022-03-03")
 def test_get_live_port_congestion():
