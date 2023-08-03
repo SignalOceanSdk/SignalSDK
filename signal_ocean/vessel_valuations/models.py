60f5e900a7f6806571b6b528784d0a991b9be4ec
Ioannis Filippas <i.filippas@thesignalgroup.com>
2023-08-03 16:47:20 +0300

    cast to dict methods added to models

--- signal_ocean/vessel_valuations/models.py
+++ signal_ocean/vessel_valuations/models.py
@@ -1,9 +1,32 @@
 # noqa: D100
+from dataclasses import dataclass, asdict
+from typing import Optional, List, Dict, Any
 
-from dataclasses import dataclass
-from datetime import datetime
-from decimal import Decimal
-from typing import Optional, List
+
+def _to_camel_case_custom(s: str,
+                          rename_keys: Optional[Dict[str, str]] = None) -> str:
+    """Transforms a string from snake_case to camel_case.
+
+    Args:
+        s: The string to transform
+        rename_keys: Key names to transform explicitly to the desired
+            target string when the default output is not adequate.
+
+    Returns:
+        The transformed string
+    """
+    _to_camelcase = s.split('_')
+    _to_camelcase = [
+        word.capitalize() if _to_camelcase.index(word) > 0
+        else word for word in _to_camelcase
+    ]
+    result = ''.join(_to_camelcase)
+
+    if rename_keys:
+        if s in rename_keys:
+            result = rename_keys[s]
+
+    return result
 
 
 @dataclass(frozen=True)
@@ -16,13 +39,28 @@ class Valuation:
         scrap_price: The estimated scrapped valuation of the vessel.
         updated_date: Date and time at which the valuation was updated.
     """
-    imo: Optional[int] = 0
-    valuation_price: Optional[Decimal] = 0
-    scrap_price: Optional[Decimal] = 0
-    updated_date: Optional[str] = None
+    imo: int
+    valuation_price: float
+    scrap_price: float
+    updated_date: str
+
+    def to_dict(self) -> Dict[Any, Any]:
+        """Cast Valuation object to dict.
+
+        Returns:
+            Dict representation of Valuation model
+
+        """
+        return asdict(
+            self,
+            dict_factory=lambda x: {
+                _to_camel_case_custom(k): v
+                for (k, v) in x if v is not None
+            })
 
 
-class HistoricalValuation(frozen=True):
+@dataclass(frozen=True)
+class HistoricalValuation:
     """A historical valuation for a specific vessel.
 
     Attributes:
@@ -30,15 +68,29 @@ class HistoricalValuation(frozen=True):
         value_from: Date and time at which the valuation was updated.
         valuation_price: The price of the valuation.
         scrap_price: The estimated scrapped valuation of the vessel.
-
     """
-    imo: Optional[int] = 0
-    value_from: Optional[str] = None
-    valuation_price: Optional[Decimal] = 0
-    scrap_price: Optional[Decimal] = 0
+    imo: int
+    value_from: str
+    valuation_price: float
+    scrap_price: float
+
+    def to_dict(self) -> Dict[Any, Any]:
+        """Cast HistoricalValuation object to dict.
+
+        Returns:
+            Dict representation of HistoricalValuation model
+
+        """
+        return asdict(
+            self,
+            dict_factory=lambda x: {
+                _to_camel_case_custom(k): v
+                for (k, v) in x if v is not None
+            })
 
 
-class PageValuations(frozen=True):
+@dataclass(frozen=True)
+class PageValuations:
     """The estimated valuations by page.
 
     Attributes:
@@ -46,13 +98,26 @@ class PageValuations(frozen=True):
         page_size:  The maximum number of results per
         page to be returned. Range between 1 and 500, by default 100.
         total: The total number of available results.
-        valuations: A list/array that contains the results of the requested vessel valuations.
+        valuations: A list/array that contains
+        the results of the requested vessel valuations.
     """
 
-    page: Optional[int] = 1
-    page_size: Optional[int] = 100
-    total: Optional[int] = 20820
-    valuations: Optional[List[Valuation]] = None
+    page: int
+    page_size: int
+    total: Optional[int]
+    valuations: List[Valuation]
 
+    def to_dict(self) -> Dict[Any, Any]:
+        """Cast PageValuations object to dict.
 
+        Returns:
+            Dict representation of PageValuations object
 
+        """
+        return {
+            "page": self.page,
+            "pageSize": self.page_size,
+            "total": self.total,
+            "valuations": [valuation.to_dict()
+                           for valuation in self.valuations]
+        }
