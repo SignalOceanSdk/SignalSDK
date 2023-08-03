b29f674db042766e5aefbf38eee9e5400607565e
Ioannis Filippas <i.filippas@thesignalgroup.com>
2023-08-03 12:51:09 +0300

    Updated valuations models for v2

--- signal_ocean/vessel_valuations/models.py
+++ signal_ocean/vessel_valuations/models.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from datetime import datetime
 from decimal import Decimal
-from typing import Optional
+from typing import Optional, List
 
 
 @dataclass(frozen=True)
@@ -12,11 +12,47 @@ class Valuation:
 
     Attributes:
         imo: The IMO number of the vessel the valuation applies to.
-        value_from: Date and time at which the valuation occurred.
         valuation_price: The price of the valuation.
+        scrap_price: The estimated scrapped valuation of the vessel.
         updated_date: Date and time at which the valuation was updated.
     """
-    imo: int
-    value_from: datetime
-    valuation_price: Optional[Decimal]
-    updated_date: datetime
+    imo: Optional[int] = 0
+    valuation_price: Optional[Decimal] = 0
+    scrap_price: Optional[Decimal] = 0
+    updated_date: Optional[str] = None
+
+
+class HistoricalValuation(frozen=True):
+    """A historical valuation for a specific vessel.
+
+    Attributes:
+        imo: The IMO number of the vessel the valuation applies to.
+        value_from: Date and time at which the valuation was updated.
+        valuation_price: The price of the valuation.
+        scrap_price: The estimated scrapped valuation of the vessel.
+
+    """
+    imo: Optional[int] = 0
+    value_from: Optional[str] = None
+    valuation_price: Optional[Decimal] = 0
+    scrap_price: Optional[Decimal] = 0
+
+
+class PageValuations(frozen=True):
+    """The estimated valuations by page.
+
+    Attributes:
+        page:  The page number you are requesting.
+        page_size:  The maximum number of results per
+        page to be returned. Range between 1 and 500, by default 100.
+        total: The total number of available results.
+        valuations: A list/array that contains the results of the requested vessel valuations.
+    """
+
+    page: Optional[int] = 1
+    page_size: Optional[int] = 100
+    total: Optional[int] = 20820
+    valuations: Optional[List[Valuation]] = None
+
+
+
