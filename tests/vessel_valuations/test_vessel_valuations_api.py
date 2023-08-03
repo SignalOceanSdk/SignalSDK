6a6a80e93e074d115493e550b9cdc1986b1a8321
Ioannis Filippas <i.filippas@thesignalgroup.com>
2023-08-03 16:47:57 +0300

    added new tests and mock objects for v2

--- tests/vessel_valuations/test_vessel_valuations_api.py
+++ tests/vessel_valuations/test_vessel_valuations_api.py
@@ -1,114 +1,64 @@
-from decimal import Decimal
-from unittest.mock import patch, MagicMock, ANY
+from unittest.mock import MagicMock
 from typing import Tuple
 
 import requests
 
 from signal_ocean.vessel_valuations import VesselValuationsAPI, Valuation
+from tests.vessel_valuations.mock_data import __mock_valuation_imo, __mock_valuation_imo_response, \
+    __mock_historical_valuations_no_args, __mock_response_historical_valuations_no_args, __mock_valuation_page, \
+    __mock_response_valuation_page, __mock_valuations_list, __mock_response_valuations_list
 from signal_ocean import Connection
 
 
-def create_valuations_api(
-        response: requests.Response) -> Tuple[VesselValuationsAPI, Connection]:
-    connection = MagicMock()
-    connection._make_get_request.return_value = response
+def create_valuations_api(response: requests.Response) -> Tuple[VesselValuationsAPI, MagicMock]:
+    connection = Connection('58546f5d3e5b45929bbae0d9ac9d831b')
+    mocked_make_request = MagicMock(return_value=response)
+    connection._make_get_request = mocked_make_request
     api = VesselValuationsAPI(connection)
+    return api, mocked_make_request
 
-    return (api, connection)
 
-def test_requests_latest_valuation_price_for_specified_IMO():
+def test_get_historical_valuations_for_imo():
     response = MagicMock()
-    response.text = None
-    api, connection = create_valuations_api(response)
-
-    api.get_latest_valuation_price(imo=123)
-
-    connection._make_get_request.assert_called_with(
-        'valuations/SnPValuation/lastvaluation/123'
-    )
+    response.json.return_value = __mock_historical_valuations_no_args
+    api, mocked_make_request = create_valuations_api(response)
+    valuation = api.get_all_historical_valuations_by_imo(imo=9414254)
+    assert valuation == __mock_response_historical_valuations_no_args
 
 
 def test_gets_latest_valuation_price():
     response = MagicMock()
-    response.text = '12.34'
-    api, *_ = create_valuations_api(response)
+    response.json.return_value = __mock_valuation_imo
+    api, mocked_make_request = create_valuations_api(response)
 
-    valuation = api.get_latest_valuation_price(imo=123)
+    valuation = api.get_latest_valuation_by_imo(imo=9414254)
 
-    assert valuation == Decimal('12.34')
+    assert valuation == __mock_valuation_imo_response
 
 
-def test_returns_None_if_there_is_no_latest_valuation_price():
+def test_returns_none_if_there_is_no_latest_valuation_price():
     response = MagicMock()
     response.status_code = requests.codes.not_found
-    api, *_ = create_valuations_api(response)
+    api, mocked_make_request = create_valuations_api(response)
 
-    valuation = api.get_latest_valuation_price(imo=123)
+    valuation = api.get_latest_valuation_by_imo(imo=123)
 
     assert valuation is None
 
 
-def test_requests_latest_valuation_for_specified_IMO():
+def test_get_latest_valuations_by_page():
     response = MagicMock()
-    response.json.return_value = {}
-    api, connection = create_valuations_api(response)
-
-    api.get_latest_valuation(imo=123)
-
-    connection._make_get_request.assert_called_with(
-        'valuations/SnPValuation/lastvaluation/shortdetails/123'
-    )
+    response.json.return_value = __mock_valuation_page
+    api, mocked_make_request = create_valuations_api(response)
+    page = api.get_latest_valuations_by_page(page=1)
+    assert page == __mock_response_valuation_page
 
 
-def test_gets_latest_valuation():
+def test_get_latest_valuations_for_list_of_vessels():
     response = MagicMock()
-    response.json.return_value = {}
-    api, *_ = create_valuations_api(response)
+    response.json.return_value = __mock_valuations_list
+    api, mocked_make_request = create_valuations_api(response)
+    valuations_list = api.get_latest_valuations_for_list_of_vessels(
+        imo_list=[9180803])
+    assert valuations_list == __mock_response_valuations_list
 
-    valuation = api.get_latest_valuation(imo=123)
-
-    assert type(valuation) is Valuation
-
-
-def test_returns_None_if_there_is_no_latest_valuation():
-    response = MagicMock()
-    response.status_code = requests.codes.not_found
-    api, *_ = create_valuations_api(response)
-
-    valuation = api.get_latest_valuation(imo=123)
-
-    assert valuation is None
-
-
-def test_requests_all_valuations_for_specified_IMO():
-    response = MagicMock()
-    response.json.return_value = []
-    api, connection = create_valuations_api(response)
-
-    api.get_valuations(imo=123)
-
-    connection._make_get_request.assert_called_with(
-        'valuations/SnPValuation/allvaluations/shortdetails/123'
-    )
-
-
-def test_gets_all_valuations():
-    response = MagicMock()
-    response.json.return_value = [{}, {}]
-    api, *_ = create_valuations_api(response)
-
-    valuations = api.get_valuations(imo=123)
-
-    assert len(valuations) == 2
-    assert type(valuations[0]) is Valuation
-    assert type(valuations[1]) is Valuation
-
-
-def test_returns_None_if_there_are_no_valuations():
-    response = MagicMock()
-    response.status_code = requests.codes.not_found
-    api, *_ = create_valuations_api(response)
-
-    valuations = api.get_valuations(imo=123)
-
-    assert valuations is None
